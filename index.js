b6e1de4b33334323761761f9b143bc49e5f53630
Antc1993 <38480247+Antc1993@users.noreply.github.com>
2023-03-15 22:39:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const morgan=require('morgan');
+require("dotenv").config();
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2)
 //Middleware
